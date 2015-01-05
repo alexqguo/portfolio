97c6f62c2b0d1f44311c71aa54a004c239b6ecb2
Alex Guo <alexqguo@gmail.com>
2015-01-04 20:13:22 -0500

    commit 2015-01-04 20:13:22 -0500

--- copy.rb
+++ copy.rb
@@ -3,7 +3,8 @@ command = "
 rsync -av --exclude=\".*\" ./ ../github-portfolio;
 cd ~/Programming/github-portfolio;
 git add -A;
-git commit -m 'commit #{time}'
+git commit -m 'commit #{time}';
+cd ~/Programming/portfolio
 "
 
 exec command
