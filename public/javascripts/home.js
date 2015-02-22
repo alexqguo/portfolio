d32d50ebd599cbaf97e6ac13aac216a35f4ff323
Alex Guo <alexqguo@gmail.com>
2015-02-22 14:11:02 -0500

    commit 2015-02-22 14:11:02 -0500

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,18 +1,37 @@
 (function() {
 	var slider = document.getElementById("slider");
 	var navLinks = document.getElementsByClassName("navlink");
-
-	function navLinkClick(e) {
-		e.preventDefault();
-		console.log(e.currentTarget.getAttribute("data-tab"));
-	}
+	var sections = document.getElementsByTagName("section");
 
 	function hideSlider() {
 		slider.className += " hidden";
 	}
 
 	function showSlider() {
-		slider.className = slider.className.replace(/\b hidden\b/g, "");
+		slider.className = "";
+	}
+
+	function displayNewTab(newTab) {
+		showSlider();
+
+		setTimeout(function() {
+			for (var i = 0; i < sections.length; i++) {
+				sections[i].className += " hide";
+			};
+
+			newTab.className = "";
+
+			hideSlider();
+		}, 500);
+
+	}
+
+	function navLinkClick(e) {
+		e.preventDefault();
+		var tabName = e.currentTarget.getAttribute("data-tab");
+		var newTab = document.getElementById(tabName);
+
+		displayNewTab(newTab);
 	}
 
 	for (var i = 0; i < navLinks.length; i++) {
