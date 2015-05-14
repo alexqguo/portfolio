cd03c2050032dfca917a5a39c6cec820b3f1162c
Alex Guo <alexqguo@gmail.com>
2015-05-13 20:32:07 -0400

    commit 2015-05-13 20:32:07 -0400

--- copy.rb
+++ copy.rb
@@ -1,6 +1,6 @@
 time = Time.now
 command = "
-rsync -av --delete --exclude=\".*\" . ../github-portfolio;
+rsync -av --exclude=\".*\" . ../github-portfolio;
 cd ~/Programming/github-portfolio;
 git add -A;
 git commit -m 'commit #{time}';
